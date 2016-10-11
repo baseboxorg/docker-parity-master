1269f900edd331de82f78aa0e3febfa11949a3a3
baseboxorg <anybucket@gmail.com>
2016-10-12 00:18:52 +0800

    update configure-parity-eth

--- configure-parity-eth.sh
+++ configure-parity-eth.sh
@@ -21,6 +21,6 @@ address=0x$(parity account list | awk 'END{print}' | tr -cd '[:alnum:]._-')
 
 DAPP_PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
  
-command="parity: parity -lsync=trace,txqueue=trace,own_tx=trace --reseal-on-txs all --force-sealing --dapps-hosts  all --dapps-interface 0.0.0.0 --dapps-port 8002 --dapps-user user --dapps-pass $DAPP_PASSWORD --chain $HOME/chain.json --author ${address} --unlock ${address} --password $HOME/.parity-pass --rpccorsdomain \"*\" --jsonrpc-hosts=all --jsonrpc-interface all >&1 1>>/var/log/parity.log 2>&1"
-echo $command >> /etc/goreman/Procfile 
+command="parity: parity -lsync=trace,txqueue=trace,own_tx=trace --reseal-on-txs all --force-sealing --dapps-hosts  all --dapps-interface 0.0.0.0 --dapps-port 8002 --dapps-user user --dapps-pass $DAPP_PASSWORD --author ${address} --unlock ${address} --password $HOME/.parity-pass --datadir=/home/vagrant/data/parity/eth --rpccorsdomain \"*\" --jsonrpc-hosts=all --jsonrpc-interface all >&1 1>>/var/log/parity.log 2>&1"
+echo $command >> /etc/goreman/Procfile
 #  --force-sealing
